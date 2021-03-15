da0b394a4c2c81020af34250878358442fd97596
msmaiaa <roxx77344@gmail.com>
2021-03-15 20:03:38 -0300

    fix wrong multiplication on interval

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopolog
   setInterval(()=>{
     console.log('Starting database populate')
     db.populateDatabase(['movies', 'shows', 'animes'])
-  },Number(process.env.CHECK_TIME_MIN) * 6000)
+  },Number(process.env.CHECK_TIME_MIN) * 60000)
 })
 .catch((e)=>{
   console.log(e);
